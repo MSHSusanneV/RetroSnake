4782218cea6aba536b843f3565c9cd842bd55ba4
Walasek <walasek@media-saturn.com>
2019-12-03 14:29:11 +0100

    Iteration 1 code complete. Ready for the first public release

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 class GameBoard {
-  constructor(width, height) {
+  constructor(width, height, speed) {
     this.width = width;
     this.height = height;
     this.canvas = document.createElement("canvas");
@@ -7,28 +7,109 @@ class GameBoard {
     this.canvas.width = width;
     this.canvas.height = height;
     this.ctx = this.canvas.getContext("2d");
-    this.ctx.fillStyle = "grey";
+    this.ctx.fillStyle = "black";
     this.ctx.fillRect(0,0,width,height);
-    this.snake = new SnakeShape(width-120, height/2, -1, 0, 10, this.ctx, width, height);
+    this.snake = null;
+    this.food = null;
     this.interval = 0;
-
+    this.score = 0;
+    this.foodWeight = 1;
+    this.gameSpeed = speed;
   }
+
   startGame() {
+    this.initGameObjects();
     this.proceedGame = this.proceedGame.bind(this);
-    this.interval = setInterval(this.proceedGame, 400);
+    this.interval = setInterval(this.proceedGame, this.gameSpeed);
   }
 
   proceedGame() {
-    this.snake.updateShape();   
+    this.snake.moveSnake();   
     if (this.snake.isCollision()) {
-      console.log ("Collision!");
       this.stopGame();
+      return;
     }
-    //check food here
+    if (this.snake.foundFood(this.food)) {
+      this.snake.eat(this.food);
+      this.score += this.food.weight;
+      document.getElementById("score").innerHTML = "Score: " + this.score;
+      let isFood = false;
+      while (!isFood) {
+        isFood = this.createFood();
+      } 
+      return;
+    } 
   }
+
   stopGame() {
     clearInterval(this.interval);
   }
+
+  initGameObjects() {
+    const boardSide = Math.floor(Math.random()*4 + 1);  //1 - top, 2 - right, 3 - bottom, 4 - left   
+    let foodx = 0;
+    let foody = 0;
+    switch (boardSide) {
+      case 1:
+        this.snake = new SnakeShape(this.canvas.width/2, 0, 
+          0, 1, 10, this.ctx, this.canvas.width, this.canvas.height);
+        foodx = this.canvas.width*0.75; 
+        foody = this.canvas.height*0.75;         
+        break;
+      case 2:
+        this.snake = new SnakeShape(this.canvas.width, (this.canvas.height - 10)/2, 
+          -1, 0, 10, this.ctx, this.canvas.width, this.canvas.height);
+        foodx = this.canvas.width*0.25;
+        foody = this.canvas.height*0.75;
+        //this.food = new Food(this.canvas.width*0.25, this.canvas.height*0.75, 10, 1, this.ctx);  
+        break;
+      case 3:
+        this.snake = new SnakeShape(this.canvas.width/2, this.canvas.height, 
+          0, -1, 10, this.ctx, this.canvas.width, this.canvas.height);
+        foodx = this.canvas.width*0.25;
+        foody = this.canvas.height*0.25;
+        //this.food = new Food(this.canvas.width*0.25, this.canvas.height*0.25, 10, 1, this.ctx);  
+        break;         
+      case 4:
+          this.snake = new SnakeShape(0, (this.canvas.height - 10)/2, 
+            1, 0, 10, this.ctx, this.canvas.width, this.canvas.height);
+          foodx = this.canvas.width*0.75;
+          foody = this.canvas.height*0.25;
+          //this.food = new Food(this.canvas.width*0.75, this.canvas.height*0.25, 10, 1, this.ctx);  
+          break;         
+      default:  
+    }
+    this.food = new Food(foodx - foodx % 10, foody - foody % 10, 10, this.foodWeight, this.ctx);  
+  }
+
+  createFood() {
+    let foodx = Math.floor(Math.random()*this.canvas.width);
+    let foody = Math.floor(Math.random()*this.canvas.height);
+    let gridx = foodx - foodx % 10;
+    let gridy = foody - foody % 10;    
+    let seg = null;
+    for (seg in this.snake.body) 
+      if (seg.x === gridx && seg.y === gridy) return false;
+    this.food = new Food(gridx, gridy, this.snake.width, this.foodWeight, this.ctx);
+    return true;
+  }
+}
+
+class Food {
+  constructor (x, y, width, weight, ctx) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.weight = weight;
+    this.ctx = ctx;
+    this.drawFood();
+  }
+  drawFood() {
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(this.x, this.y, this.width, this.width);
+  }
+
+
 }
 
 class ElementPosition {
@@ -47,19 +128,7 @@ class MovingCurve {
     this.xdir = xdir; 
     this.ydir = ydir;
     this.head = new ElementPosition (x, y);
-    this.body =  [this.head,
-      new ElementPosition (this.head.x + this.width, this.head.y),
-      new ElementPosition(this.head.x + this.width*2, this.head.y), 
-      new ElementPosition(this.head.x + this.width*3, this.head.y),
-      new ElementPosition(this.head.x + this.width*4, this.head.y),
-      new ElementPosition(this.head.x + this.width*5, this.head.y),
-      new ElementPosition(this.head.x + this.width*6, this.head.y),
-      new ElementPosition(this.head.x + this.width*7, this.head.y),
-      new ElementPosition(this.head.x + this.width*8, this.head.y),
-      new ElementPosition(this.head.x + this.width*9, this.head.y),
-      new ElementPosition(this.head.x + this.width*10, this.head.y),                  
-      new ElementPosition(this.head.x + this.width*11, this.head.y)];   
-    
+    this.body =  [this.head];
 
     document.onkeydown = e => {
       switch (e.keyCode) {
@@ -93,17 +162,32 @@ class SnakeShape extends MovingCurve {
     super (x, y, xdir, ydir, elementSize, ctx);
     this.xmax = boardWidth;
     this.ymax = boardHeight;
+    this.lastTail = null;
   }
 
-  updateShape() {
-    const tail = this.moveCurve();
+  moveSnake() {
+    this.lastTail = this.moveCurve();
     this.ctx.fillStyle = "green";
     this.ctx.fillRect(this.head.x, this.head.y, this.width, this.width);
-    this.ctx.fillStyle = "grey";
-    this.ctx.fillRect(tail.x, tail.y, this.width, this.width); 
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(this.lastTail.x, this.lastTail.y, this.width, this.width); 
   }
 
-  eat() {
+  foundFood(food) {
+    if (this.head.x === food.x && this.head.y === food.y) {
+      //console.log ("Mahlzeit!");
+      return true;
+    }
+    return false;
+  }
+
+  eat(food) {
+    //console.log ("Danke!");
+    this.body.push(this.lastTail);
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(this.lastTail.x, this.lastTail.y, this.width, this.width); 
+
+
   }
 
   isCollision() {
@@ -119,6 +203,6 @@ class SnakeShape extends MovingCurve {
   }
 }
 
-let game = new GameBoard(480, 270);
+let game = new GameBoard(280, 170, 200);   //(480, 270);
 game.startGame();
 
